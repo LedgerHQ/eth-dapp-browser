6a86cb291eff673d8de86db6fab2f54458e207af
Alexandre Chabrolin <87011321+achabrolin-ledger@users.noreply.github.com>
2021-12-30 13:07:39 +0100

    Refetch accounts if new one added with requestAccount

--- src/DAPPBrowser/index.tsx
+++ src/DAPPBrowser/index.tsx
@@ -341,7 +341,14 @@ export function DAPPBrowser({
       };
       if (ledgerAPIRef.current) {
         const account = await ledgerAPIRef.current.requestAccount(payload);
-        selectAccount(account);
+
+        /**
+         * If requestAccount returns an empty account object (i.e: all fields are null or undifined,
+         * especillay the id), this means a new account has been added and we should refetch the accounts list
+         *
+         * FIXME: this whole flow / hanldeling of newly added accounts could be improved by extending SDK capabilities
+         */
+        account.id ? selectAccount(account) : fetchAccounts();
       }
     } catch (error) {
       // TODO: handle error
