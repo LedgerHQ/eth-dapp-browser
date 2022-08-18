8eeff2c89e31165b5deb4e99a66e3a95f4164bd5
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-08-18 17:21:33 +0200

    Forward query params to dapp
    Removing dapp-browser specific one beforehand

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,75 @@
-import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { DAPPBrowser } from "../src/DAPPBrowser";
 import { ChainConfig } from "../src/DAPPBrowser/types";
-import { getQueryVariable } from "../src/helpers";
+import { getFirstValueFromArray } from "../src/helpers";
 
-function DappBrowserPage() {
+const DappBrowserPage = (): JSX.Element | null => {
   const [mounted, setMounted] = useState(false);
   const router = useRouter();
-
-  const theme = getQueryVariable("theme", router);
-  const initialAccountId = getQueryVariable("accountId", router);
-  const rawParams = getQueryVariable("params", router);
-
-  const params = rawParams ? JSON.parse(rawParams) : {};
-  const chainConfigs: ChainConfig[] = params.networks;
-  const nanoApp = params.nanoApp;
-  const dappUrl = params.dappUrl;
-  const dappName = params.dappName || "DApp";
 
   useEffect(() => {
     setMounted(true);
     return () => setMounted(false);
   }, []);
 
-  if (mounted) {
-    return dappUrl ? (
-      <DAPPBrowser
-        dappName={dappName}
-        dappUrl={dappUrl}
-        nanoApp={nanoApp}
-        theme={theme}
-        chainConfigs={chainConfigs}
-        initialAccountId={initialAccountId}
-      />
-    ) : null;
+  // Early return if the page is not mounted yet
+  if (!mounted) {
+    return null;
   }
-  return null;
-}
+
+  /**
+   * We segregate the dapp browser specific query params from the dapp query params.
+   */
+  const {
+    params: dappBrowserParams,
+    accountId,
+    ...dappQueryParams
+  } = router.query;
+
+  /**
+   * We process the dapp browser specific query params.
+   */
+  /**
+   * TODO: Handle the case where the dapp provides and uses query params also used by the dapp browser.
+   * Example: if the dapp provides a query param named "accountId" and the dapp browser also uses this query param
+   * As of today,only the first value will be taken into account and the rest will be discarded.
+   * Also, the values used by the dapp browser will not (and should not) be
+   * forwarded to the dapp (like params used by the dapp browser itself, or other
+   * live specific params such as accountId).
+   */
+  const rawParams = getFirstValueFromArray(dappBrowserParams);
+  const initialAccountId = getFirstValueFromArray(accountId);
+
+  const params = rawParams ? JSON.parse(rawParams) : {};
+
+  const {
+    networks: chainConfigs,
+    nanoApp,
+    dappUrl,
+    dappName = "DApp",
+  }: {
+    networks: ChainConfig[];
+    nanoApp: string;
+    dappUrl: string;
+    dappName: string;
+  } = params;
+
+  // Early return if no dapp provided (no dappUrl available in the query)
+  if (!dappUrl) {
+    return null;
+  }
+
+  return (
+    <DAPPBrowser
+      dappQueryParams={dappQueryParams}
+      dappName={dappName}
+      dappUrl={dappUrl}
+      nanoApp={nanoApp}
+      chainConfigs={chainConfigs}
+      initialAccountId={initialAccountId}
+    />
+  );
+};
 
 export default DappBrowserPage;
