4d26b222e88eb3096b51f0e42ab40626dccb46d3
Hugo MOROSINI <hugo.morosini@gmail.com>
2023-04-26 18:41:52 +0200

    fixed theme issue

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,5 +1,3 @@
-import { ColorPalette } from "@ledgerhq/ui-shared";
-
 export const defaultTheme = {
   type: "light",
   colors: {
@@ -11,47 +9,5 @@ export const defaultTheme = {
     warning: "#ff7701",
   },
 };
-
-declare module "styled-components" {
-  export interface Font {
-    weight: number;
-    style: string;
-  }
-  export interface DefaultTheme {
-    theme: string;
-    sizes: {
-      topBarHeight: number;
-      sideBarWidth: number;
-      drawer: {
-        side: {
-          big: {
-            width: number;
-          };
-          small: {
-            width: number;
-          };
-        };
-        popin: {
-          min: {
-            height: number;
-            width: number;
-          };
-          max: {
-            height: number;
-            width: number;
-          };
-        };
-      };
-    };
-    radii: number[];
-    fontFamilies: Record<string, Record<string, Font>>;
-    fontSizes: number[];
-    space: number[];
-    shadows: string[];
-    colors: ColorPalette & { palette: ColorPalette };
-    fontWeights: Record<string, string>;
-    zIndexes: number[];
-  }
-}
 
 export default defaultTheme;
